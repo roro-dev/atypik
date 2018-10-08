dc07e3f7e2ad4ab4f859438829f923e40b8c49b7
Stefane Rodrigues <stefane.rodrigues@AEF.local>
2018-10-08 14:13:09 +0200

    mise en place connexion TO DO

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use App\Entity\Utilisateur;
+
+class SecurityController extends AbstractController
+{
+
+	public function login(AuthenticationUtils $authenticationUtils)
+	{	
+		$repository = $this->getDoctrine()->getRepository(Utilisateur::class);
+		$users = $repository->findAll();
+	    // get the login error if there is one
+	    $error = $authenticationUtils->getLastAuthenticationError();
+
+	    // last username entered by the user
+	    $lastUsername = $authenticationUtils->getLastUsername();
+
+	    return $this->render('security/login.html.twig', array(
+	        'last_username' => $lastUsername,
+	        'error'         => $error,
+	    ));
+	}
+}
