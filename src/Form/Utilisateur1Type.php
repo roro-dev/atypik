c928cfe858d221da89db272b26d37c7bcd528c65
roro-dev <stefanerodrigues75010@gmail.com>
2018-11-05 11:28:42 +0100

    Debut coté BO

---
+++ src/Form/Utilisateur1Type.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Utilisateur;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class Utilisateur1Type extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('prenom')
+            ->add('adresse')
+            ->add('email')
+            ->add('telephone')
+            ->add('password')
+            ->add('valideUser')
+            ->add('tokenUser')
+            ->add('id_role')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Utilisateur::class,
+        ]);
+    }
+}
