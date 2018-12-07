e4b11b59587f5950703eb03970cb59b5e379f559
Stefane Rodrigues <stefane.rodrigues@AEF.local>
2018-12-07 16:11:57 +0100

    controller api

---
+++ src/Controller/api/ApiController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller\api;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+* @Route("/api", name="home")
+*/
+class ApiController extends AbstractController
+{
+    public function connexion_post(Request $request) {
+
+    }
+}
+    
