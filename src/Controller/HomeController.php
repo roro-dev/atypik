faac5a03628d73fe7f5fcc7f30f4647470902e59
roro-dev <stefanerodrigues75010@gmail.com>
2018-12-20 17:02:14 +0100

    logement + icon nav

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,6 +7,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\LogementRepository;
 use App\Entity\TypeLogement;
 use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Logement;
 
 class HomeController extends AbstractController
 {
@@ -18,7 +19,7 @@ class HomeController extends AbstractController
             'type' => (!empty($request->request->get('type'))) ? $request->request->get('type') : 0,
             'ville' => (!empty($request->request->get('ville'))) ? $request->request->get('ville') : '',
             'depart' => (!empty($request->request->get('depart'))) ? $request->request->get('depart') : date('d/m/Y'),
-            'arrivee' => (!empty($request->request->get('arrivee'))) ? $request->request->get('arrivee') : date('d/m/Y'),
+            'arrivee' => (!empty($request->request->get('arrivee'))) ? $request->request->get('arrivee') : date('d/m/Y', strtotime('+1 day'))
         );
         $repo = $this->getDoctrine()->getRepository(TypeLogement::class);
         return $this->render('home/index.html.twig', [
@@ -34,13 +35,17 @@ class HomeController extends AbstractController
         $data = array(
             'type' => (!empty($request->request->get('type'))) ? $request->request->get('type') : 0,
             'ville' => (!empty($request->request->get('ville'))) ? $request->request->get('ville') : '',
+            'nb' => (!empty($request->request->get('nb'))) ? $request->request->get('nb') : '',
             'depart' => (!empty($request->request->get('depart'))) ? $request->request->get('depart') : date('d/m/Y'),
-            'arrivee' => (!empty($request->request->get('arrivee'))) ? $request->request->get('arrivee') : date('d/m/Y'),
+            'arrivee' => (!empty($request->request->get('arrivee'))) ? $request->request->get('arrivee') : date('d/m/Y', strtotime('+1 day'))
         );
         $repo = $this->getDoctrine()->getRepository(TypeLogement::class);
+        $repoSearch = $this->getDoctrine()->getRepository(Logement::class);
+        $logements = $repoSearch->findByCriteres(array('type' => $request->request->get('type'), 'nb' => $request->request->get('nb')));
         return $this->render('home/recherche.html.twig', [
             'types' => $repo->findAll(),
-            'data' => $data
+            'data' => $data,
+            'logements' => $logements
         ]);
     }
 }
