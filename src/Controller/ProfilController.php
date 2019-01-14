4c3d6bc92c42249bb1e2db941b9a2e8cb7946313
Sasorith David <davidsaso10@gmail.com>
2019-01-14 10:23:23 +0100

    Mise à jour page Dashboard #21

---
+++ src/Controller/ProfilController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\LogementRepository;
+use App\Entity\RolesUtilisateur;
+
+Class ProfilController extends AbstractController {
+
+    /**
+     * @Route("/profil", name="profil")
+     */
+    public function index() {
+        return $this->render('home/profil.html.twig');
+    }
+}
