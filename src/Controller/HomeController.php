ab1ba2abdf67be792c6b179fc3792334649090dd
roro-dev <stefanerodrigues75010@gmail.com>
2019-01-16 19:06:19 +0100

    amélioration code

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -17,17 +17,9 @@ class HomeController extends AbstractController
      * @Route("/", name="home")
      */
     public function index(Request $request) {
-        $data = array(
-            'type' => (!empty($request->request->get('type'))) ? $request->request->get('type') : 0,
-            'ville' => (!empty($request->request->get('ville'))) ? $request->request->get('ville') : '',
-            'nb' => (!empty($request->request->get('nb'))) ? $request->request->get('nb') : '',
-            'depart' => (!empty($request->request->get('depart'))) ? $request->request->get('depart') : date('d/m/Y'),
-            'arrivee' => (!empty($request->request->get('arrivee'))) ? $request->request->get('arrivee') : date('d/m/Y', strtotime('+1 day'))
-        );
-        $repo = $this->getDoctrine()->getRepository(TypeLogement::class);
         return $this->render('home/index.html.twig', [
-            'types' => $repo->findAll(),
-            'data' => $data
+            'types' => $this->getDoctrine()->getRepository(TypeLogement::class)->findAll(),
+            'data' => array('type' => 0, 'ville' => '', 'nb' => 1, 'depart' => date('d/m/Y'),'arrivee' => date('d/m/Y', strtotime('+1 day')))
         ]);
     }
 
