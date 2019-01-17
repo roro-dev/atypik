f58d2585011200401cc07e3b61c55bdda1073b57
roro-dev <stefanerodrigues75010@gmail.com>
2019-01-17 20:25:30 +0100

    maj profil

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -5,19 +5,31 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Reservation;
+use App\Form\ProfilType;
 
 Class ProfilController extends AbstractController {
 
     /**
-     * @Route("/profil", name="profil")
+     * @Route("/profil", name="profil", methods="post|get")
      * @Security("has_role('ROLE_USER')")
      */
-    public function index() {
+    public function index(Request $request) {
+        
+        $form = $this->createForm(ProfilType::class, $this->getUser());
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            if($form->isValid()) {
+                $this->addFlash('success', 'Votre compte à bien été modifié.');
+            } else {
+                $this->addFlash('error', 'Une erreur est survenue. Veuillez contactez l\'administrateur du site.');
+            }
+        }
         return $this->render('home/profil.html.twig', [
             'newResas' => $this->getDoctrine()->getRepository(Reservation::class)->findProchainesReservations($this->getUser(), date('Y-m-d')),
-            'oldResas' => $this->getDoctrine()->getRepository(Reservation::class)->findAnciennesReservations($this->getUser(), date('Y-m-d'))
-
+            'oldResas' => $this->getDoctrine()->getRepository(Reservation::class)->findAnciennesReservations($this->getUser(), date('Y-m-d')),
+            'form' => $form->createView()
         ]);
     }
 }
---
+++ src/Form/ProfilType.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Utilisateur;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+
+class ProfilType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom', TextType::class, array(
+                'attr' => array(
+                    'class' => 'form-control'
+                ),
+                'label' => 'Nom *',
+                'label_attr' => array(
+                    'class' => 'col-2 col-form-label'
+                )
+            ))
+            ->add('prenom', TextType::class, array(
+                'attr' => array(
+                    'class' => 'form-control'
+                ),
+                'label' => 'Prénom *',
+                'label_attr' => array(
+                    'class' => 'col-2 col-form-label'
+                )
+            ))
+            ->add('email', EmailType::class, array(
+                'attr' => array(
+                    'class' => 'form-control'
+                ),
+                'label' => 'Adresse mail *',
+                'label_attr' => array(
+                    'class' => 'col-2 col-form-label'
+                )
+            ))
+            /*->add('plainPassword', RepeatedType::class, array(
+                'type' => PasswordType::class,
+                'first_options'  => array(
+                    'label' => 'Mot de passe',
+                    'attr' => array(
+                        'class' => 'form-control'
+                    )
+                ),
+                'second_options' => array(
+                    'label' => 'Répéter mot de passe',
+                    'attr' => array(
+                        'class' => 'form-control'
+                    )
+                ),
+            ))*/
+            ->add('telephone', TextType::class, array(
+                'attr' => array(
+                    'class' => 'form-control'
+                ),
+                'label' => 'Téléphone *',
+                'label_attr' => array(
+                    'class' => 'col-2 col-form-label'
+                )
+            ))
+            ->add('adresse', TextType::class, array(
+                'attr' => array(
+                    'class' => 'form-control'
+                ),
+                'label' => 'Adresse *',
+                'label_attr' => array(
+                    'class' => 'col-2 col-form-label'
+                )
+            ))
+            ->add('newsletter', CheckboxType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => Utilisateur::class,
+        ));
+    }
+}
